d07b2e421da88884cccc362cb93a06e26ec6013b
rn9dfj3 <hazcyqund84@yahoo.co.jp>
2018-10-24 20:56:17 +0900

    [Add]Save Auto
    Add saving automatically option

--- timebank.py
+++ timebank.py
@@ -14,10 +14,11 @@
 # limitations under the License.
 import bpy
 from pathlib import Path
+from datetime import datetime, timedelta
 bl_info = {
     "name": "Timebank",
     "author": "rn9dfj3",
-    "version": (0, 0),
+    "version": (1, 0),
     "blender": (2, 79, 0),
     "location": "Info > File",
     "description": "Save blender file named each process.",
@@ -38,6 +39,7 @@ ANIMATING = 4
 RENDERING = 5
 COMPOSITING = 6
 SCRIPTING = 7
+ALL = 8
 MODELING_ICON = 'EDIT'
 SKINNING_ICON = 'WPAINT_HLT'
 TEXTURING_ICON = 'TEXTURE'
@@ -46,11 +48,17 @@ ANIMATING_ICON = 'ACTION'
 RENDERING_ICON = 'RENDER_STILL'
 COMPOSITING_ICON = 'NODETREE'
 SCRIPTING_ICON = 'TEXT'
+PROCESS = 0
+RATIO = 1
 
 def save(self, context, type):
+        pref = context.user_preferences.addons[__name__].preferences
         filepath = context.blend_data.filepath
         if filepath == "":
             bpy.ops.wm.save_as_mainfile('INVOKE_AREA')
+            #start save auto
+            if pref.saveauto and not pref.saveauto_run:
+                bpy.ops.wm.timebank_save_auto('INVOKE_DEFAULT')            
             return
         path = Path(filepath)
         suffix = path.suffix
@@ -89,7 +97,9 @@ def save(self, context, type):
         filepath = str(path)
         self.report({'INFO'}, "Saved "+str(path))
         bpy.ops.wm.save_as_mainfile(filepath=filepath, check_existing=True)
-
+        #start save auto
+        if pref.saveauto and not pref.saveauto_run:
+            bpy.ops.wm.timebank_save_auto('INVOKE_DEFAULT')
 
 class SaveModeling(bpy.types.Operator):
     bl_idname = "wm.timebank_modeling"
@@ -273,6 +283,147 @@ class LoadLatest(bpy.types.Operator):
 #                bpy.ops.wm.open_mainfile(filepath=str(max_file))
 #        return {'FINISHED'}
 
+class SaveAuto(bpy.types.Operator):
+    bl_idname = "wm.timebank_save_auto"
+    bl_label = "Save Auto"
+    bl_description = "Save the file automatically"
+    bl_options = {'REGISTER'}
+    start = None
+    processes = []
+    _timer = None
+    
+    def execute(self, context):
+        #self.report({"INFO"}, "1")
+        self._timer = None
+        context.user_preferences.addons[__name__].preferences.saveauto_run = False
+        return {'FINISHED'}
+    
+    def modal(self, context, event):
+        #context.area.tag_redraw()
+        if not context.user_preferences.addons[__name__].preferences.saveauto:
+            self._timer = None
+            context.user_preferences.addons[__name__].preferences.saveauto_run = False
+            return {'FINISHED'}
+        if not event.type == 'TIMER':
+            return {'PASS_THROUGH'}
+        screen = bpy.context.screen
+        window = bpy.context.window
+        window_area = window.width * window.height
+        #x = event.mouse_x
+        #y = event.mouse_y
+        current = datetime.now()
+        end = self.start + timedelta(seconds=1)
+#        if current > end:
+        #self.report({"INFO"}, ".")
+        #self.report({"INFO"}, bpy.context.mode)
+        process_area = {MODELING:0.0, SKINNING:0.0, TEXTURING:0.0, MORPHING:0.0, ANIMATING:0.0, RENDERING:0.0, COMPOSITING:0.0, SCRIPTING:0.0}
+        for area in screen.areas:
+            area_area = area.width * area.height
+            ratio = area_area / window_area
+            if area.type in ["TIMELINE", "GRAPH_EDITOR", "DOPESHEET_EDITOR", "NLA_EDITOR"]:
+                process_area[ANIMATING] += ratio
+            if area.type in ["IMAGE_EDITOR"]:
+                process_area[TEXTURING] += ratio
+            if area.type in ["TEXT_EDITOR", "CONSOLE"]:
+                process_area[SCRIPTING] += ratio
+            if area.type in ["IMAGE_EDITOR", "NODE_EDITOR"]:
+                process_area[COMPOSITING] += ratio
+            if area.type in ["SEQUENCE_EDITOR", "CLIP_EDITOR"]:
+                process_area[RENDERING] += ratio
+            if area.type in ["VIEW_3D"]:
+                #self.report({"INFO"}, "c"+context.mode)                        
+                #self.report({"INFO"}, "b"+bpy.context.mode)                        
+                if context.mode in ["EDIT_MESH", "EDIT_CURVE", "EDIT_SURFACE", "EDIT_TEXT", "EDIT_METABALL", "EDIT_LATTICE", "SCULPT","PARTICLE"]:
+                    process_area[MODELING] += ratio
+                if context.mode in ["EDIT_ARMATURE", "PAINT_WEIGHT"]:
+                    process_area[SKINNING] += ratio
+                if context.mode in ["POSE"]:
+                    process_area[SKINNING] += ratio
+                    process_area[ANIMATING] += ratio                    
+                if context.mode in ["PAINT_TEXTURE"]:
+                    process_area[TEXTURING] += ratio
+        self.processes.append(process_area)
+            #h = area.x <= x and x < area.x + area.width
+            #v = area.y <= y and y < area.y + area.height
+            #if h and v:
+            #    self.report({"INFO"}, ".")
+            #    #print(area.type)
+            #    if area.type in ["TIMELINE", "GRAPH_EDITOR", "DOPESHEET_EDITOR", "NLA_EDITOR"]:
+            #        self.processes.append(ANIMATING)
+            #    if area.type in ["IMAGE_EDITOR"]:
+            #        self.processes.append(TEXTURING)
+            #    if area.type in ["TEXT_EDITOR", "CONSOLE"]:
+            #        self.processes.append(SCRIPTING)
+            #    if area.type in ["NODE_EDITOR"]:
+            #        self.processes.append(COMPOSITING)
+            #    if area.type in ["SEQUENCE_EDITOR", "CLIP_EDITOR"]:
+            #        self.processes.append(RENDERING)
+            #    if area.type in ["VIEW_3D"]:
+            #        self.processes.append(MODELING)                        
+        self.start = current
+        num = context.user_preferences.addons[__name__].preferences.saveauto_interval
+        if len(self.processes) >= num:
+            ps = [[n, 0.0] for n in range(ALL)]                
+            for process in  self.processes:
+                for p in range(ALL):
+                    ps[p][RATIO] += process[p]
+            ranks = sorted(ps,  key=lambda p: p[1], reverse=True)
+            first = ranks[0][PROCESS]
+            second = ranks[1][PROCESS]
+            ave_second = ranks[1][RATIO] / num
+            if ave_second > 0.33:
+                p1 = min(first, second)
+                p2 = max(first, second)
+                if p1 == MODELING:
+                    self.save(context, second)
+                else:
+                    self.save(context, first)
+            else:
+                self.save(context, first)
+            self.processes = []
+        #return {'PASS_THROUGH'}        
+        return {'RUNNING_MODAL'}
+
+    def invoke(self, context, event):
+        #self.report({"INFO"}, "1")
+        context.user_preferences.addons[__name__].preferences.saveauto_run = True
+        self.start = datetime.now()
+        self.processes = []
+        self._timer = context.window_manager.event_timer_add(1.0, context.window)
+        context.window_manager.modal_handler_add(self)
+        return {'RUNNING_MODAL'}
+
+    def save(self, context, index):
+        filepath = context.blend_data.filepath
+        if filepath == "":
+            self.report({"WARNING"}, "Auto save was failed. Please save the file manually once.")
+            return
+        if index == MODELING:
+            #SaveModeling.execute(context)
+            bpy.ops.wm.timebank_modeling()
+        if index == SKINNING:
+            #SaveSkinning.execute(context)
+            bpy.ops.wm.timebank_skinning()
+        if index == TEXTURING:
+            #SaveTexturing.execute(context)
+            bpy.ops.wm.timebank_texturing()
+        if index == MORPHING:
+            #SaveMorphing.execute(context)
+            bpy.ops.wm.timebank_morphing()
+        if index == ANIMATING:
+            #SaveAnimating.execute(context)
+            bpy.ops.wm.timebank_animating()
+        if index == RENDERING:
+            #SaveRendering.execute(context)
+            bpy.ops.wm.timebank_rendering()
+        if index == COMPOSITING:
+            #SaveCompositing.execute(context)
+            bpy.ops.wm.timebank_compositing()
+        if index == SCRIPTING:
+            #SaveScripting.execute(context)
+            bpy.ops.wm.timebank_scripting()
+        self.report({"INFO"}, "Saved the file automatically.")
+        
 class TimebankLoadMenu(bpy.types.Menu):
     bl_label = "Load Timebank"
     bl_idname = "INFO_MT_timebank_load_menu"
@@ -376,8 +527,28 @@ def menu_fn(self, context):
     self.layout.operator(SaveRendering.bl_idname, icon=RENDERING_ICON)
     self.layout.operator(SaveCompositing.bl_idname, icon=COMPOSITING_ICON)
     self.layout.operator(SaveScripting.bl_idname, icon=SCRIPTING_ICON)
-
-
+    self.layout.operator(SaveAuto.bl_idname, icon=SCRIPTING_ICON)
+    
+class TIMEBANK_Preferences(bpy.types.AddonPreferences):
+    bl_idname = __name__
+    saveauto = bpy.props.BoolProperty(name="Save Auto", description="Save the file automatically")
+    saveauto_run = bpy.props.BoolProperty(name="Save Auto Run", description="Whether autosave run or not", default=False, options={"HIDDEN", "SKIP_SAVE"})
+    saveauto_interval = bpy.props.IntProperty(name="Interval", description="Interval for saving automatically in seconds", min=1, default=180)
+    
+    def draw(self, context):
+        layout = self.layout
+        #layout.label("Test")
+        #row = layout.row()
+        row = layout.row()        
+        row.prop(self, "saveauto")
+        row.label("The starting need saving manually once.", icon="ERROR")
+        column = layout.column()
+        column.prop(self, "saveauto_interval")
+        column.enabled = not context.user_preferences.addons[__name__].preferences.saveauto_run
+                
+        #if self.saveauto and not self.saveauto_run:
+        #    #SaveAuto.execute(self, context)
+        #    bpy.ops.wm.timebank_save_auto('INVOKE_DEFAULT')            
 def register():
     bpy.utils.register_module(__name__)
     bpy.types.INFO_MT_file.append(menu_fn)
